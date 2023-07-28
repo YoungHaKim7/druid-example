fad5629039eeb4a359fbf932ef1bd69e3d429565
GYoung <ytok1108@gmail.com>
2023-07-28 15:59:45 +0900

    button Click & Interaction widgets

---
+++ examples/est_kor_time/est_kor_time05_interaction_widgets/src/main.rs
@@ -0,0 +1,43 @@
+use druid::widget::{Button, Container, Flex, Label, List, Split};
+use druid::{AppLauncher, Color, Widget, WindowDesc};
+use im::{vector, Vector};
+
+type TodoList = Vector<String>;
+
+fn build_ui() -> impl Widget<TodoList> {
+    Split::columns(
+        Container::new(
+            // Dynamic list of Widgets
+            List::new(|| Label::dynamic(|data, _| format!("List item: {data}"))),
+        )
+        .border(Color::grey(0.6), 2.0),
+        Container::new(
+            Flex::column()
+                .with_flex_child(
+                    Button::new("Add item").on_click(|_, data: &mut Vector<String>, _| {
+                        data.push_back("New item".into())
+                    }),
+                    1.0,
+                )
+                .with_flex_child(Label::new("Textbox placeholder"), 1.0),
+        )
+        .border(Color::grey(0.6), 3.0),
+    )
+}
+
+fn main() {
+    let main_window = WindowDesc::new(build_ui())
+        .window_size((600.0, 400.0))
+        .title("My first Druid App");
+    let initial_data = vector![
+        "first item".into(),
+        "second item".into(),
+        "third item".into(),
+        "foo".into(),
+        "bar".into(),
+    ];
+
+    AppLauncher::with_window(main_window)
+        .launch(initial_data)
+        .expect("Failed to launch application");
+}
